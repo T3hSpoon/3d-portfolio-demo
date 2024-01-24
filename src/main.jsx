87d98840f9d59dc976f206b8302d0dad4eea2176
T3hspoon <toader.ctin.ovidiu@gmail.com>
2024-01-24 22:38:59 +0200

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
-import ReactDOM from 'react-dom/client'
-import App from './app'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './app';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
