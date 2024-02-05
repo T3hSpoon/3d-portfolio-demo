f93d13bb1abc24eb38e862091918b0f7156df13d
T3hspoon <toader.ctin.ovidiu@gmail.com>
2024-02-05 14:13:47 +0200

    update constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -32,11 +32,7 @@ export const skills = [
     name: 'CSS',
     type: 'Frontend',
   },
-  {
-    imageUrl: express,
-    name: 'Express',
-    type: 'Backend',
-  },
+
   {
     imageUrl: git,
     name: 'Git',
@@ -57,46 +53,13 @@ export const skills = [
     name: 'JavaScript',
     type: 'Frontend',
   },
-  {
-    imageUrl: mongodb,
-    name: 'MongoDB',
-    type: 'Database',
-  },
-  {
-    imageUrl: motion,
-    name: 'Motion',
-    type: 'Animation',
-  },
-  {
-    imageUrl: mui,
-    name: 'Material-UI',
-    type: 'Frontend',
-  },
-  {
-    imageUrl: nextjs,
-    name: 'Next.js',
-    type: 'Frontend',
-  },
-  {
-    imageUrl: nodejs,
-    name: 'Node.js',
-    type: 'Backend',
-  },
+
   {
     imageUrl: react,
     name: 'React',
     type: 'Frontend',
   },
-  {
-    imageUrl: redux,
-    name: 'Redux',
-    type: 'State Management',
-  },
-  {
-    imageUrl: sass,
-    name: 'Sass',
-    type: 'Frontend',
-  },
+
   {
     imageUrl: tailwindcss,
     name: 'Tailwind CSS',
@@ -111,8 +74,8 @@ export const skills = [
 
 export const experiences = [
   {
-    title: 'React.js Developer',
-    company_name: 'Starbucks',
+    title: 'React Developer',
+    company_name: 'Brai',
     icon: starbucks,
     iconBg: '#accbe1',
     date: 'March 2020 - April 2021',
@@ -173,62 +136,28 @@ export const socialLinks = [
   {
     name: 'GitHub',
     iconUrl: github,
-    link: 'https://github.com/YourGitHubUsername',
+    link: 'https://github.com/t3hspoon',
   },
   {
     name: 'LinkedIn',
     iconUrl: linkedin,
-    link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+    link: 'https://www.linkedin.com/in/constantin-ovidiu-toader-5b9032298',
   },
 ];
 
 export const projects = [
   {
-    iconUrl: pricewise,
-    theme: 'btn-back-red',
-    name: 'Amazon Price Tracker',
-    description:
-      'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
-    link: 'https://github.com/adrianhajdin/pricewise',
-  },
-  {
-    iconUrl: threads,
-    theme: 'btn-back-green',
-    name: 'Full Stack Threads Clone',
-    description:
-      'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-    link: 'https://github.com/adrianhajdin/threads',
-  },
-  {
-    iconUrl: car,
-    theme: 'btn-back-blue',
-    name: 'Car Finding App',
-    description:
-      'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
-    link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
-  },
-  {
-    iconUrl: snapgram,
-    theme: 'btn-back-pink',
-    name: 'Full Stack Instagram Clone',
-    description:
-      'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
-    link: 'https://github.com/adrianhajdin/social_media_app',
-  },
-  {
     iconUrl: estate,
     theme: 'btn-back-black',
-    name: 'Real-Estate Application',
-    description:
-      'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
-    link: 'https://github.com/adrianhajdin/projects_realestate',
+    name: 'Designious Pro',
+    description: 'Developed a web application for e-commerce.',
+    link: 'https://designious-pro.vercel.app/',
   },
   {
     iconUrl: summiz,
     theme: 'btn-back-yellow',
-    name: 'AI Summarizer Application',
-    description:
-      'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
-    link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+    name: '3D Portfolio',
+    description: 'One version of my portfolio.',
+    link: '/',
   },
 ];
